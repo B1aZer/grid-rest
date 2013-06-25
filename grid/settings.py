112eb50e11b8596339b8e7a7502dc45e8e16824a
Dmitry Branitskiy <dmitry.branitskiy@gmail.com>
2013-06-25 19:35:50 +0400

    database addon

--- grid/settings.py
+++ grid/settings.py
@@ -162,10 +162,11 @@ REST_FRAMEWORK = {
     'PAGINATE_BY': 10
 }
 
-
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+import os
+if os.environ.get('HEROKU'):  # heroku config:set HEROKU=1
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
