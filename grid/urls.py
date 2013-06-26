b566f074ecf73b9add5edeca6381c810d4808465
Dmitry Branitskiy <dmitry.branitskiy@gmail.com>
2013-06-26 20:33:14 +0400

    trash removal

--- grid/urls.py
+++ grid/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url, include
 from rest_framework import routers
-from quickstart import views
+from grid import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 # Uncomment the next two lines to enable the admin:
